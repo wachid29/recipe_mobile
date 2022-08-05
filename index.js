f3a35dd5f20d84f50c241235c37a78cdde891cdf
wachid <wachidnurafif29@gmail.com>
2022-08-05 17:40:25 +0700

    add gitignore

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 8001;
+const port = process.env.PORT || 8002;
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
 const cors = require("cors");
